ba24ee259343bc57af88f0e3c2689fb69d46f85a
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-02-07 00:48:48 +0900

    mod

---
+++ app/assertion_signing_key_pair_generator.py
@@ -0,0 +1,19 @@
+from jwcrypto import jwk
+import json
+from typing import Tuple
+import os
+
+
+def key_pair_generate() -> Tuple[dict, dict]:
+    key = jwk.JWK.generate(kty='RSA', alg='RS256', use='sig', size=2048)
+
+    private_key =key.export_private()
+    public_key = key.export_public()
+
+    return private_key, public_key
+
+
+if __name__ == '__main__':
+    private_key, public_key = key_pair_generate()
+    print("=== private key ===\n"+json.dumps(json.loads(private_key),indent=2))
+    print("=== public key ===\n"+json.dumps(json.loads(public_key),indent=2))   
