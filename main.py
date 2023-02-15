c798bec966d55a11a8a959fed39be31189532df0
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-02-16 00:57:57 +0900

    Update: 20230216125757

--- main.py
+++ main.py
@@ -49,20 +49,20 @@ def res_data(text):
         
     for record in data:
         if record[1] == text:
-            res += f"【{record[1]}】\n"
-            res += f"図鑑No.{record[0]}\n"
-            res += f"第{record[2]}世代\n" if re.findall(r'\d', record[2]) else f"{record[2]}\n"
-            res += f"タイプ:{record[3]}/{record[4]}\n" if len(record[4]) else f"タイプ:{record[3]}/-\n"
-            res += f"HP:{record[5]}\n"
-            res += f"攻撃:{record[6]}\n"
-            res += f"防御:{record[7]}\n"
-            res += f"特攻:{record[8]}\n"
-            res += f"特防:{record[9]}\n"
-            res += f"素早さ:{record[10]}\n"
-            res += f"合計:{record[11]}\n"
-            res += f"特性1:{record[12]}\n" if len(record[12]) else f"特性1:-\n"
-            res += f"特性2:{record[13]}\n" if len(record[13]) else f"特性2:-\n"
-            res += f"夢特性:{record[14]}" if len(record[14]) else f"夢特性:-"
+            res += "\n".join((f"【{record[1]}】",
+                              f"図鑑No.{record[0]}",
+                              f"第{record[2]}世代" if re.findall(r'\d', record[2]) else f"{record[2]}",
+                              f"タイプ:{record[3]}/{record[4]}",
+                              f"HP:{record[5]}",
+                              f"攻撃:{record[6]}",
+                              f"防御:{record[7]}",
+                              f"特攻:{record[8]}",
+                              f"特防:{record[9]}",
+                              f"素早さ:{record[10]}",
+                              f"合計:{record[11]}",
+                              f"特性1:{record[12]}",
+                              f"特性2:{record[13]}",
+                              f"夢特性:{record[14]}"))
             break
         elif text in (record[12], record[13], record[14]):
             have_trait = f"{record[1]}(夢)" if text in record[14] else record[1]
@@ -72,12 +72,10 @@ def res_data(text):
     
     if len(have_trait_list):
         res += f"【特性：「{text}」を持つポケモン】\n"
-        for have_traits in have_trait_list:
-            res += have_traits if have_traits == have_trait_list[len(have_trait_list)-1] else f"{have_traits}\n"
+        res += "\n".join(have_traits for have_traits in have_trait_list)
     elif len(have_alias_list):
         res += "以下のワードで検索してください。\n"
-        for have_aliases in have_alias_list:
-            res += have_aliases if have_aliases == have_alias_list[len(have_alias_list)-1] else f"{have_aliases}\n"
+        res += "\n".join(have_aliases for have_aliases in have_alias_list)
 
     if not len(res):
         res += "マッチするポケモン・特性が見つかりませんでした。\n(現在第7世代までのポケモンのみサポートしています。)"
