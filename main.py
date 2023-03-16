faec2d66631cd6b1969e8308a91321b5c6afd56b
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-03-17 00:59:09 +0900

    Update: 20230317125909

--- main.py
+++ main.py
@@ -57,16 +57,16 @@ def res_data(text: str) -> dict:
             break
         elif text in (record[3], record[4]) or re.findall(r'^{}\s{}$'.format(record[3], record[4]), text) or re.findall(r'^{}\s{}$'.format(record[4], record[3]), text):
             have_type_dict |= {record[1]: record[18]}
-            #if len(have_type_dict) > 50:
-                #break
+            if len(have_type_dict) > 50:
+                break
         elif text in (record[12], record[13], record[14]):
             have_trait_dict |= {record[1]: record[18]}
-            #if len(have_trait_dict) > 50:
-                #break
+            if len(have_trait_dict) > 50:
+                break
         elif text in (record[15], record[16]):
             have_egg_group_dict |= {record[1]: record[18]}
-            #if len(have_egg_group_dict) > 50:
-                #break
+            if len(have_egg_group_dict) > 50:
+                break
         elif re.findall(r'^{}\(.+\)$'.format(text), record[1]):
             have_alias_list.append(record[1])
 
