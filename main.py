17c38b7bf316036b5d0ba6f029c2aeaaced70412
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-11-11 17:27:54 +0900

    remove healthcheck

--- main.py
+++ main.py
@@ -42,11 +42,6 @@ def callback():
     return 'OK'
 
 
-@app.route("/", methods=['GET'])
-def healthcheck():
-    return 'Healthcheck OK'
-
-
 def res_data(text: str) -> dict:
     with open('./master/master.csv', 'r') as f:
         data = [row for row in csv.reader(f)]
